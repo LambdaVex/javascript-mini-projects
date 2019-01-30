4de8447c50a0844e7fd094fca60902f5da95cf8b
Ali Denno <aliedenno@gmail.com>
2019-01-30 19:39:25 +0100

    Update github.js

--- GitHubFinder/github.js
+++ GitHubFinder/github.js
@@ -2,7 +2,7 @@ class Github {
   constructor() {
     this.client_id = 'd9308aacf8b204d361fd';
     this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
-    this.repos_count = 5;
+    this.repos_count = 5; // Only last 5 
     this.repos_sort = 'created: asc';
   }
 

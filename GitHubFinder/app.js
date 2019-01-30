733e9a524d99b2e0f45e711c712f87383717b102
Ali Denno <aliedenno@gmail.com>
2019-01-30 15:10:42 +0100

    Update app.js and add a UI

--- GitHubFinder/app.js
+++ GitHubFinder/app.js
@@ -1,5 +1,7 @@
 // Init Github
 const github = new Github;
+// Init UI
+const ui = new UI;
 
 // Search input
 const searchUser = document.getElementById('searchUser');
@@ -18,6 +20,7 @@ searchUser.addEventListener('keyup', (e) => {
 
       } else {
         // Show profile
+        ui.showProfile(data.profile);
       }
     })
   } else {
